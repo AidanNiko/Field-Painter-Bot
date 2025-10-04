a306f6667c77ca9141495c2edd5d0f7c4a802048
Aidan Nikolaus <aniko030@uottawa.ca>
2025-10-03 22:47:07 -0400

    major adjustments

---
+++ FieldPainterBot2/app/src/main/java/com/example/fieldpainterbot/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.example.fieldpainterbot;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+}
+
+
---
+++ FieldPainterBot2/app/src/main/java/com/example/fieldpainterbot/HomepageActivity.java
@@ -0,0 +1,27 @@
+package com.example.fieldpainterbot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage); // Your layout XML file
+
+        Button button = findViewById(R.id.button_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomepageActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
