d2ebe027c3c367171f9d01c1e4789cce9c9ca4c9
Jean Diomande <97854305+hatsu23@users.noreply.github.com>
2025-10-17 22:26:56 -0400

    Add comments to ConnectionStatus enum
    Added comments to clarify the purpose of the enum values.

--- FieldPainterBot2/app/src/main/java/com/example/fieldpainterbot/ConnectionStatus.java
+++ FieldPainterBot2/app/src/main/java/com/example/fieldpainterbot/ConnectionStatus.java
@@ -1,7 +1,11 @@
 package com.example.fieldpainterbot;
-
+// Represent the possible state of a Bluetooth connection.
+// Used to track and communicate connection status throughout the app
 public enum ConnectionStatus {
+    // Indicates that the device is successfully connected
     CONNECTED,
+    // Indicates that a connection attempt is in progress
     CONNECTING,
+    // Indicates that the device is not connected
     DISCONNECTED
 }
